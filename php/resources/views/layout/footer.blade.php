d15c471c38e8e646d6fb66640840ee4b29be2528
Vinicius de Santana <vinicius.vts@gmail.com>
2019-05-25 21:06:54 -0300

    jquery antes

--- php/resources/views/layout/footer.blade.php
+++ php/resources/views/layout/footer.blade.php
@@ -24,8 +24,8 @@
     </div>
 </footer>
     <!--JavaScript at end of body for optimized loading-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
     $(document).ready(function(){
         M.updateTextFields();
